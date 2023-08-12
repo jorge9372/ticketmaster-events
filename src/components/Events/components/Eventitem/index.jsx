c0b0b47f69f37e9a446362c8fd64bf161ea904a7
jorge9372 <jorge9372@gmail.com>
2023-08-12 18:04:54 -0400

    feat: Se crea events.json que simula la informacion de una API, se prueba el funcionamiento

--- src/components/Events/components/Eventitem/index.jsx
+++ src/components/Events/components/Eventitem/index.jsx
@@ -1,5 +1,11 @@
-const Eventitem = () => {
-    return <div>Event item</div>
+const EventItem = ({ info, name, image }) => {
+    return (
+        <div>
+            <img src={image} alt={name} width={200} />
+            <h4>{name}</h4>
+            <p>{info}</p>
+        </div>
+    );
 };
 
-export default Eventitem;
+export default EventItem;
--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,10 +1,23 @@
-import Eventitem from "./components/Eventitem";
+import EventItem from "./components/Eventitem";
+import data from "../../data/events.json";
+
+const { _embedded: { events } } = data;
+
 
 const Events = () => {
+    const eventsComponent = events.map((eventItem) => (
+        <EventItem
+            key={`event-item-${eventItem.id}`}
+            name={ eventItem.name}
+            info={ eventItem.info }
+            image={ eventItem.images[0].url}
+        />
+    ));
+
     return (
         <div>
             Eventos
-            <Eventitem/>
+            {eventsComponent}
         </div>
     );
 
