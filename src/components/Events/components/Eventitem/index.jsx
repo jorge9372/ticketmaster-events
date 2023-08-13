069abaaff12d45c1a75c1962c55b31db2c483eb8
jorge9372 <jorge9372@gmail.com>
2023-08-13 12:00:56 -0400

    Feat: usando modulos de CSS en React

--- src/components/Events/components/Eventitem/index.jsx
+++ src/components/Events/components/Eventitem/index.jsx
@@ -1,16 +1,20 @@
+import styles from './EventItem.module.css';
+
 const EventItem = ({ info, id, name, image, onEventClick }) => {
-    
-const handleSeeMoreClick = (evt) => {
-    evt.stopPropagation();
-    onEventClick(id);
-}
+
+    const handleSeeMoreClick = (evt) => {
+        evt.stopPropagation();
+        onEventClick(id);
+    }
 
     return (
-        <div onClick={()=> console.log('padresito click')}>
-            <img src={image} alt={name} width={200} />
-            <h4>{name}</h4>
-            <p>{info}</p>
-            <button onClick={handleSeeMoreClick}>Ver mas</button>
+        <div onClick={() => console.log('padresito click')} className={styles.eventItemContainer}>
+            <img src={image} alt={name} width={200} height={200} />
+            <div className={styles.eventInfoContainer}>
+                <h4 className={styles.eventName}>{name}</h4>
+                <p className={styles.eventInfo}>{info}</p>
+                <button onClick={handleSeeMoreClick} className={styles.seeMoreBtn}>Ver mas</button>
+            </div>
         </div>
     );
 };
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,12 +5,9 @@ const Navbar = ({ onSearch }) => {
 
     useEffect(() => { console.log('onSearch cambio') }, [onSearch]);
 
-    useEffect(()=>{console.log('componente listo')},[]);
+    useEffect(() => { console.log('componente listo') }, []);
 
-    useEffect(()=>{console.log('search cambio')},[search]);
-
-
-
+    useEffect(() => { console.log('search cambio') }, [search]);
 
     const handleInputChange = (event) => {
         setSearch(event.target.value);
@@ -22,14 +19,32 @@ const Navbar = ({ onSearch }) => {
     };
 
     return (
-        <div>
-            <p>Mi boletera</p>
-            <input
-                placeholder="Busca tu evento favorito"
-                onChange={handleInputChange}
-                onKeyDown={handleInputKeyDawn}
-                value={search}
-            />
+        <div style={{
+            marginBottom: 14,
+            width: '100%',
+            display: 'flex',
+        }}>
+            <div style={{ flex: 1, display: 'flex' }}>
+                <p style={{
+                    fontSize: 24,
+                    fontWeight: 'bold',
+                }}>Mi boletera</p>
+            </div>
+            <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                <input
+                    placeholder="Busca tu evento favorito"
+                    onChange={handleInputChange}
+                    onKeyDown={handleInputKeyDawn}
+                    value={search}
+                    style={{
+                        fontSize: 16,
+                        borderRadius: 4,
+                        padding: '6px 12px',
+                        border: 'none',
+                        width: 200,
+                    }}
+                />
+            </div>
         </div>
     );
 };
